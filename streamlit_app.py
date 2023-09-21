f820db267abf87d1a38615ec84d37fe79e097a34
Suchhana <145699071+Suchhana@users.noreply.github.com>
2023-09-21 22:21:37 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('my parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.Text('Omega 3 & Blueberry Oatmeal')
+streamlit.Text('Kale,spinach & rocket smoothie')
+streamlit.Text('Hard boiled free-range egg')
+streamlit.Text
